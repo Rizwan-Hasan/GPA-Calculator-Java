a9c0136072c9a0f47cb63772629d56b4398133ef
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-05-05 00:18:03 +0600

    updated

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,4 +1,22 @@
 package app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
 public class Controller {
+
+    @FXML
+    private Button calculateButton;
+
+    @FXML
+    private MenuItem quitMenuBtn;
+
+    @FXML
+    public void ok (ActionEvent event) {
+        System.out.println("Quitting");
+        System.exit(0);
+    }
+
 }
