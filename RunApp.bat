b18ee90cbd3104e4ed27c95273ccaf65a2bc3132
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-05-09 00:37:57 +0600

    updated

---
+++ RunApp.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar "CGPA_Calculator.jar"
+pause
---
+++ src/app/Controller.java
@@ -0,0 +1,74 @@
+package app;
+
+import app.gsheetedit.GradeSheetEditor;
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import java.util.Optional;
+
+public class Controller extends ControllerAnimation
+{
+    @FXML
+    public void quitMenuBtnAction (ActionEvent event)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Are you sure to quit?");
+        alert.setContentText(null);
+
+        alert.getDialogPane().setGraphic(new ImageView(new Main().questionIcon));
+
+        alert.initOwner(calculateButton.getScene().getWindow());
+        ButtonType yesButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+        ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
+        alert.getButtonTypes().setAll(yesButton, noButton);
+        Optional<ButtonType> alertStatus = alert.showAndWait();
+        if (alertStatus.isPresent() && alertStatus.get() == yesButton) {
+            System.out.println("Quit");
+            Platform.exit();
+        }
+        event.consume();
+    }
+
+    @FXML
+    public void calculateBtnAction (ActionEvent event)
+    {
+        calculator cgpa = new calculator (
+                theoryBox.getText().strip(),
+                labBox.getText().strip(),
+                theoryCheckBox.isSelected(),
+                labCheckBox.isSelected()
+        );
+
+        String result = String.valueOf(cgpa.getCGPA());
+        mainLabel.setText("Your cgpa is: " + result);
+
+        // Main Label Fade In
+        {
+            FadeTransition fadeIn = new FadeTransition();
+            fadeIn.setDuration(Duration.seconds(1));
+            fadeIn.setNode(mainLabel);
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.setCycleCount(1);
+            fadeIn.setAutoReverse(false);
+            fadeIn.playFromStart();
+        }
+        event.consume();
+    }
+
+    @FXML
+    public void editSheetBtnAction(ActionEvent event)
+    {
+        GradeSheetEditor x = new GradeSheetEditor();
+        x.showWindow();
+        event.consume();
+    }
+
+}
