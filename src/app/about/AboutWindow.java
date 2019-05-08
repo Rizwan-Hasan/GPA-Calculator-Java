b85915df607e03df410494d2cfcdf36ebea1ea41
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-05-09 02:56:15 +0600

    added

---
+++ src/app/about/AboutWindow.java
@@ -0,0 +1,32 @@
+package app.about;
+
+import app.Main;
+import app.resources;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AboutWindow
+{
+    public void showWindow ()
+    {
+        try {
+            // Window Opening
+            FXMLLoader aboutWindowFXML = new FXMLLoader(getClass().getResource("AboutWindow.fxml"));
+            Parent root = aboutWindowFXML.load();
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().clear();
+            if (new Main().isWindows())
+                scene.getStylesheets().add(getClass().getResource("AboutWindow.css").toExternalForm());
+            stage.setTitle("About");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.getIcons().add(new resources().aboutIcon);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
